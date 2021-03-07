40a76afebcca9bfbb9aaa9ca74ce4b12a98b6f0c
Praveen Jaiswal <praveen.jaiswal@lidolearning.com>
2021-03-07 19:02:42 +0530

    first automation script

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element, ElementFinder, ElementArrayFinder } from 'protractor';
+var jobUrl = 'http://localhost:4200/job/'
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,10 +11,46 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('galytix-test-exercise app is running!');
+    expect(page.getPageHeader()).toEqual('BROWSE JOBS')
   });
 
+  it('should have working anchor tag', () => {
+    element(by.tagName('ul')).all(by.tagName('li > a'))
+      .each((job, index) => {
+        if (index === 0)
+          if (job != undefined && job.getText != null) {
+            job.getAttribute('href').then((jobLink) => {
+              expect(jobLink).toEqual(`${jobUrl + (index + 1)}`)
+              job.click()
+              expect(browser.getCurrentUrl()).toEqual(`${jobUrl + (index + 1)}`);
+              page.navigateBack()
+              expect(browser.getCurrentUrl()).toContain(`${jobUrl.slice(0, -1) + 's'}`);
+              expect(page.getPageHeader()).toEqual('BROWSE JOBS')
+            });
+          }
+      });
+  });
+
+  it('perform end to end application submission', () => {
+    element(by.tagName('ul')).all(by.tagName('li > a'))
+      .each((job, index) => {
+        if (job != undefined && job.getText != null) {
+          expect(browser.getCurrentUrl()).toEqual(`http://localhost:4200/jobs`);
+          expect(page.getPageHeader()).toEqual('BROWSE JOBS')
+          job.click()
+          expect(browser.getCurrentUrl()).toEqual(`http://localhost:4200/job/${index + 1}`);
+          element(by.tagName('button')).click()
+          page.fillApplicationForm();
+          expect(page.getApplicationStatus()).toEqual('Application submitted')
+          page.navigateBack()
+        }
+      });
+  });
+
+
+
   afterEach(async () => {
+    browser.driver.sleep(3000);
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
+var path = require('path');
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  navigateBack(): Promise<unknown> {
+    return browser.navigate().back() as Promise<unknown>;
   }
+
+  getPageHeader(): Promise<string> {
+    return element(by.tagName('h1')).getText() as Promise<string>;
+  }
+
+
+  fillApplicationForm() {
+    let name: ElementFinder = element(by.name('name'))
+    let email: ElementFinder = element(by.name('email'))
+
+    name.clear().then(() => {
+      name.sendKeys('Praveen');
+    });
+
+    email.clear().then(() => {
+      email.sendKeys('lala@hjg.co');
+    });
+
+    let fileToUpload = '../null.pdf',
+      absolutePath = path.resolve(__dirname, fileToUpload);
+    element(by.name('resume')).sendKeys(absolutePath);
+  }
+
+  getApplicationStatus(): Promise<string> {
+    let applyNowButton: ElementFinder = element(by.className('ml-auto')).element(by.tagName('button'))
+    applyNowButton.click();
+    return element(by.className('success')).getText() as Promise<string>;
+  }
+
+
 }
